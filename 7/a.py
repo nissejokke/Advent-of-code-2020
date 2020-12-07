a97b5ddf7c47bfd4a5a4c3b2e89eb08f6a7524ac
Johan Sundberg <johan.sundberg@futureordering.com>
2020-12-07 10:42:56 +0100

    Day 7

---
+++ 7/a.py
@@ -0,0 +1,41 @@
+input = open("7/input.txt", "r").read()
+
+# input = """
+# light red bags contain 1 bright white bag, 2 muted yellow bags.
+# dark orange bags contain 3 bright white bags, 4 muted yellow bags.
+# bright white bags contain 1 shiny gold bag.
+# muted yellow bags contain 2 shiny gold bags, 9 faded blue bags.
+# shiny gold bags contain 1 dark olive bag, 2 vibrant plum bags.
+# dark olive bags contain 3 faded blue bags, 4 dotted black bags.
+# vibrant plum bags contain 5 faded blue bags, 6 dotted black bags.
+# faded blue bags contain no other bags.
+# dotted black bags contain no other bags.
+# """
+
+lines = input.strip().split("\n")
+rules = []
+for line in lines:
+    words = line.split(" ")
+    parentbag = " ".join(words[0:2])
+    rest = " ".join(words[4:])
+    childstrings = rest.split(", ")
+    for childstr in childstrings:
+        childwords = childstr.split(" ")
+        if childwords[0] == "no":
+            childcount = 0
+        else:
+            childcount = int(childwords[0])
+            childbag = " ".join(childwords[1:3])
+
+            rules.append({ "parent": parentbag, "child": childbag })
+
+matches = set()
+
+def find_bag(bag: str):
+    for rule in rules:
+        if rule["child"] == bag:
+            matches.add(rule["parent"])
+            find_bag(rule["parent"])
+
+find_bag("shiny gold")
+print(len(matches))
