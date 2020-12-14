70b7f3f46a381d46f6302496a651eb8c9fdbea86
Johan Sundberg <johan.sundberg@futureordering.com>
2020-12-14 08:19:19 +0100

    Day 3 b

--- 13/b.py
+++ 13/b.py
@@ -1,14 +1,39 @@
 input = open("13/input.txt", "r").read()
+# 1890502625454599
+#
 
 # input = """
 # 939
 # 7,13,x,x,59,x,31,19
 # """
+# 1068781
+# 3162341
 
-input = """
-939
-1789,37,47,1889
-"""
+# input = """
+# 939
+# 1789,37,47,1889
+# """
+# 1202161486
+# 5465967480
+# 5876813119
+
+# input = """
+# 111
+# 17,x,13,19
+# """
+
+# input = """
+# 111
+# 13,17,19
+# """
+# 3417
+# 2992
+# 4199
+t=2
+13 2 (% 13)
+1 * t = 2
+
+2%13
 
 lines = input.strip().split("\n")
 
@@ -23,22 +48,32 @@ for line in rawlines:
 buslines = sorted(buslines, key=lambda x: x[1], reverse=True)
 print(buslines)
 t = 0
-# for busline in buslines:
-#     t*=busline[1]
-# print(t)
+
+n = 1
+for busline in buslines:
+    n*=busline[1]
+print("upper:", n)
+
+from datetime import datetime
+time = datetime.now()
+
+busline_count = len(buslines)
 while True:
     p = buslines[0]
     t += p[1] # 59
     off = p[0] # 4
     n = 0
-    for i in range(1, len(buslines)):
+    for i in range(1, busline_count):
         boff,bid = buslines[i]
         if (t + boff - off) % bid == 0:
             n += 1
         else:
             break
-    if n == len(buslines) - 1:
+    if n == busline_count - 1:
         break
-    # if t % 1000 == 0:
-    #     print(t)
+    
+    if (datetime.now() - time).seconds > 3:
+        time = datetime.now()
+        print(t)
+
 print(t - buslines[0][0])
